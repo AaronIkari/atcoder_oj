5e1b98209ecaa982f601d1d00b4ac07d1268467e
AaronIkari <iamikari0383@gmail.com>
2019-04-17 13:43:41 +0800

    abc122, A - Double Helix, Log: 1AC

---
+++ abc122/A.py
@@ -0,0 +1,7 @@
+# A - Double Helix
+# https://atcoder.jp/contests/abc122/tasks/abc122_a
+
+bases_dict = {'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
+base = raw_input()
+
+print '{}'.format(bases_dict[base])
