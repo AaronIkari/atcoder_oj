a70b67dab8586cf301b0cb6d32385ca745f96b2b
AaronIkari <iamikari0383@gmail.com>
2019-04-19 00:48:22 +0800

    abc120, K-th Common Divisor, Log: 1WA, 1AC

--- abc120/B.py
+++ abc120/B.py
@@ -5,37 +5,16 @@ import math
 
 MAX = 100
 
-def gcd(A, B):
-    if B > A:
-        A, B = A, B
-
-    if B == 0:
-        return A
-
-    return gcd(B, A-(A/B)*B)
-
-prime_table = [True]*MAX
-prime_table[0] = prime_table[1] = False
-for i in range(2, int(math.sqrt(MAX)) + 1):
-    for j in range(i*2, MAX, i):
-        prime_table[j] = False
-primes = [i_idx for i_idx in range(len(prime_table)) if prime_table[i_idx] == True]
+def gcd(m, n):
+    return m if n == 0 else gcd(n, m % n)
 
 A, B, K = map(int, raw_input().split())
 gcdAB = gcd(A, B)
 
-factors = set()
-factors.add(gcdAB)
-factors.add(1)
-
-for prime in primes:
-    if prime > gcdAB:
-        break
-    if gcdAB % prime == 0:
-        factors.add(prime)
-        factors.add(gcdAB/prime)
-
-factors = list(factors)
-factors.sort(reverse=True)
-
-print '{}'.format(factors[K - 1])
+cnt = 0
+for n in range(gcdAB, 0, -1):
+    if gcdAB % n == 0:
+        cnt += 1
+        if cnt == K:
+            print '{}'.format(n)
+            break
