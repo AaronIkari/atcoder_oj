1040411f08e0fac869580a1693842fe283aa424e
AaronIkari <iamikari0383@gmail.com>
2019-04-19 11:02:06 +0800

    abc119, A - Still TBD, Log: 1AC

---
+++ abc119/A.py
@@ -0,0 +1,12 @@
+# A - Still TBD
+# https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+# threshold
+TY, TM, TD = 2019, 4, 30
+
+Y, M, D = map(int, raw_input().split('/'))
+
+if M > 4:
+    print '{}'.format('TBD')
+else:
+    print '{}'.format('Heisei')
