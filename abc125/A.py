a5e69ee9032880edf127141defae8a4510c4dec0
AaronIkari <iamikari0383@gmail.com>
2019-04-28 13:49:05 +0800

    add title and link

--- abc125/A.py
+++ abc125/A.py
@@ -1,3 +1,5 @@
-A, B, T = map(int, raw_input().split())
+# A - Biscuit Generator
+# https://atcoder.jp/contests/abc125/tasks/abc125_a
 
+A, B, T = map(int, raw_input().split())
 print '{}'.format( int((T+0.5)/A) * B)
